fd937492be6dc657aace9d9b72416cb3f0f6a76e
Devanshu Sharma <devanshusharma@Devanshus-MacBook-Air.local>
2023-07-07 17:57:31 +0530

    website updated

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-        var tablinks = document.getElementsByClassName("tab-links");
-        var tabcontents = document.getElementsByClassName("tab-contents");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-        function opentab(tabname){
-            for(tablink of tablinks)
-            {
-                tablink.classList.remove("active-link");
-            }
-            for(tabcontent of tabcontents)
-            {
-                tabcontent.classList.remove("active-tab");
-            }
-            event.currentTarget.classList.add("active-link");
-            document.getElementById(tabname).classList.add("active-tab");
-        }
+function opentab(tabname){
+    for(tablink of tablinks)
+    {
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents)
+    {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
 
-        var sm = document.getElementById("sidemenu");
+var sm = document.getElementById("sidemenu");
 
-        function openmenu(){
-            sm.style.right = "0";
-        }
-        function closemenu(){
-            sm.style.right = "-200px";
-        }
+function openmenu(){
+    sm.style.right = "0";
+}
+function closemenu(){
+    sm.style.right = "-200px";
+}
 
-        window.addEventListener('scroll', reveal);
-
+//  BOTTOM TO UP SUBTLE ANIMATION FOR CONTENT
+window.addEventListener('scroll', reveal);
 function reveal(){
     var reveals = document.querySelectorAll('.reveal');
 
@@ -42,7 +42,7 @@ function reveal(){
     }
 }
 
-// Automatic Typing 
+// Automatic Typing in front page
 const typed = new Typed('.multiple-text', {
     strings : ['Programmer.','Frontend Developer.', 'Java Developer.', 'Youtuber.', 'Basketball lover.'],
     typeSpeed : 80,
